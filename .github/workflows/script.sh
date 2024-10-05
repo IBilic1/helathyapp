cd0f42f5d8c1289d9aadf3e7d03f5109f39a929d
Ivana Bilić <bilic.ivana11@gmail.com>
2024-10-06 01:10:10 +0200

    Update script.sh

--- .github/workflows/script.sh
+++ .github/workflows/script.sh
@@ -5,6 +5,12 @@ image=$2
 namespace=$3
 
 deploy=`oc get deployment $app_name`
+if oc get deployment "$app_name" -n "$namespace" >/dev/null 2>&1; then
+  echo "Deployment '$app_name' already exists in namespace '$namespace'."
+else
+  echo "Creating deployment '$app_name' in namespace '$namespace'..."
+  oc create deployment "$app_name" --image=my-image:latest -n "$namespace"
+
 if [[ "$?" -eq 0 ]]; then
     oc set image deployment/$app_name $app_name=$image
     oc rollout restart deployment/$app_name
