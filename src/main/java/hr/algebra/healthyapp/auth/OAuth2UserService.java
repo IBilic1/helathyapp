4437909c8501fc313919e89a112b9a2f75c7c793
ibilic1 <bilic.ivana11@gmail.com>
2024-10-12 16:37:57 +0200

    [IT-0002] TRY TO FIX

--- src/main/java/hr/algebra/healthyapp/auth/OAuth2UserService.java
+++ src/main/java/hr/algebra/healthyapp/auth/OAuth2UserService.java
@@ -38,7 +38,7 @@ public class OAuth2UserService extends DefaultOAuth2UserService {
     }
 
     private OAuth2User processOAuth2User(OAuth2UserRequest oAuth2UserRequest, OAuth2User oAuth2User) {
-        System.out.println("IVANA");
+        System.out.println("EVA");
         Optional<Object> oEmail = Optional.ofNullable(oAuth2User.getAttributes().get("email"));
         if (oEmail.isEmpty()) {
             throw new OAuth2AuthenticationException("[ERROR] Email is required");
